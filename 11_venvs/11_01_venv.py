c6b2f93fb0b74ded3b761212b2dec383a773b4a9
shalonningram <mscianika@gmail.com>
2021-05-19 17:17:18 -0400

    completed labs

--- 11_venvs/11_01_venv.py
+++ 11_venvs/11_01_venv.py
@@ -10,3 +10,28 @@ In your CodingNomads folder create a new folder. Inside of that folder:
 7. Create a new virtual environment and install the packages from the requirements.txt file.
 
 '''
+# completed in the command line
+
+# name of virtual environment : myvenv
+# install files
+pandas
+numpy
+matplotlib
+
+# requirements file
+cycler==0.10.0
+kiwisolver==1.3.1
+matplotlib==3.4.2
+numpy==1.20.3
+pandas==1.2.4
+Pillow==8.2.0
+pyparsing==2.4.7
+python-dateutil==2.8.1
+pytz==2021.1
+six==1.16.0
+
+# deactivate
+deactivate
+
+# remove directory
+rm -rf virtualenv
