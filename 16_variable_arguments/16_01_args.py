f83eb1ddcc9487aad14a29500874a44160f57528
shalonningram <mscianika@gmail.com>
2021-05-20 11:50:33 -0400

    completed labs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+# from my coursework notes
+
+def print_args(*args):
+    for a in args:
+        print(a)
+print_args('barcelona', 'malta', 'ubud')
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+# from the coursework notes
+
+def print_kwargs(**kwargs):
+    for k, v in kwargs.items():
+        print(k, v)
+
+print_kwargs(country = 'indonesia', city = 'ubud')
