879fbeab4a272905c8b9d16cfb7098039b1afcbc
ShalonnIngram <mscianika@gmail.com>
2021-05-26 21:18:47 -0400

    completed lab

--- APIs_and_Databases/APIs/create_post_req.py
+++ APIs_and_Databases/APIs/create_post_req.py
@@ -1,4 +1,4 @@
-'''
+
 Write the necessary code to make a POST request to:
 
     http://demo.codingnomads.co:8080/tasks_api/users
@@ -7,4 +7,18 @@ and create a user with your information.
 
 Make a GET request to confirm that your user information has been saved.
 
-'''
+
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+body = {
+    "first_name": "[Shalonn]",
+    "last_name": "[Ingram]",
+    "email": "[mscianika@gmail.com]"
+}
+
+response = requests.post(base_url, json=body)
+print(f"Response Code: {response.status_code}")
+
+r = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
+print()
