4541e7458dc2c4a87d1a402c613577cd12028cdf
ShalonnIngram <mscianika@gmail.com>
2021-05-27 10:45:17 -0400

    completed lab

--- APIs_and_Databases/APIs/create_delete_req.py
+++ APIs_and_Databases/APIs/create_delete_req.py
@@ -1,6 +1,10 @@
-'''
+
 Write a program that makes a DELETE request to remove the user your create in a previous example.
 
-Again, make a GET request to confirm that information has been deleted.
+Again make a GET request to confirm that information has been deleted.
 
-'''
+
+import requests
+
+r = requests.delete("http://demo.codingnomads.co:8080/tasks_api/users/363")
+print(r.status_code)
