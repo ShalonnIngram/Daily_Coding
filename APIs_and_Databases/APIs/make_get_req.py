f6803caee65caa244489e09c8c155fd5aa2e1946
ShalonnIngram <mscianika@gmail.com>
2021-05-27 09:08:06 -0400

    initial commit

--- APIs_and_Databases/APIs/make_get_req.py
+++ APIs_and_Databases/APIs/make_get_req.py
@@ -3,8 +3,3 @@ Building on the previous example, create a list of all of the emails of the user
 the list to the console.
 
 
-import requests
-from pprint import pprint
-
-r = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
-print()
