d598d411becc8a22fdd099aa5fa5ebde8d2f5454
ShalonnIngram <mscianika@gmail.com>
2021-05-27 10:38:04 -0400

    completed lab

--- APIs_and_Databases/APIs/print_emails.py
+++ APIs_and_Databases/APIs/print_emails.py
@@ -1,5 +1,15 @@
-'''
+
 Building on the previous example, create a list of all of the emails of the users and print
 the list to the console.
 
-'''
+
+
+import requests
+from pprint import pprint
+
+r = requests.get("http://demo.codingnomads.co:8080/tasks_api/users")
+info = r.json()
+
+# loop through nested json data to return all emails
+for i in info["data"]:
+    pprint(i["email"])
