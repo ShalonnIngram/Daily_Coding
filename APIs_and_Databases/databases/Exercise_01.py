e7fa0de4c7c6e2e31740cfe204e149f5d76cbd4b
ShalonnIngram <mscianika@gmail.com>
2021-05-27 13:17:34 -0400

    completed lab

--- APIs_and_Databases/databases/Exercise_01.py
+++ APIs_and_Databases/databases/Exercise_01.py
@@ -1,7 +1,20 @@
-'''
+
 
 All of the following exercises should be done using sqlalchemy.
 
 Using the provided database schema, write the necessary code to print information about the film and category table.
 
-'''
+
+import sqlalchemy                                                                            
+import pymysql                                                                               
+from pprint import pprint                                                                    
+                                                                                             
+engine = sqlalchemy.create_engine("mysql+pymysql://root:trublue6@localhost/sakila")          
+connection = engine.connect()                                                                
+metadata = sqlalchemy.MetaData()                                                             
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)               
+category = sqlalchemy.Table('category', metadata, autoload=True, autoload_with=engine)       
+                                                                                             
+pprint(film)                                                                                 
+pprint(category)                                                                             
+                                                                                             
