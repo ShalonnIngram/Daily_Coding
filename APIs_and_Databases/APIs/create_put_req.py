75a37bdccd65c844e0ead22e2e4036d34e4de787
ShalonnIngram <mscianika@gmail.com>
2021-05-26 21:20:24 -0400

    completed lab

--- APIs_and_Databases/APIs/create_put_req.py
+++ APIs_and_Databases/APIs/create_put_req.py
@@ -1,6 +1,32 @@
-'''
+
 Write a program that makes a PUT request to update your user information to a new first_name, last_name and email.
 
 Again make a GET request to confirm that your information has been updated.
 
-'''
+
+
+
+import requests
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+
+# update existing record with PUT request
+body = {
+    "id": 361,
+    "first_name": "Cianika",
+    "last_name": "Campbell",
+    "email": "mscianika2@email.com"
+}
+
+# execute the PUT request
+response = requests.put(base_url, json=body)
+
+# print the status code
+print(f"Response Code: {response.status_code}")
+
+# make a GET request to retrieve the new data from the server
+response = requests.get(base_url)
+
+# print the updated record
+print(f"Response Content: {response.content}")
