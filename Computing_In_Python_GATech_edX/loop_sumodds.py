cef47a9c0df4bd29922b4fb4e75525f18fea8848
ShalonnIngram <mscianika@gmail.com>
2021-06-10 23:11:54 -0400

    Create loop_sumodds.py

---
+++ Computing_In_Python_GATech_edX/loop_sumodds.py
@@ -0,0 +1,14 @@
+mystery_int = 50
+
+#Add some code below that will find and print the sum of
+#every odd number between 0 and mystery_int. This time,
+#exclude the bounds (e.g. if mystery_int was 51, add the odds
+#from 1 to 49, but not 51).
+#
+#Hint: There are multiple ways to do this!
+
+
+#Add your code here!
+
+odd_total = sum(range(1, mystery_int, 2))
+print(odd_total)
