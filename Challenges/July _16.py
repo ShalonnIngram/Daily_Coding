9f3042c08bdb365b91fb42a1a41dc543f13bc3fb
ShalonnIngram <mscianika@gmail.com>
2021-07-16 11:06:36 -0400

    Update July _16.py

--- Challenges/July _16.py
+++ Challenges/July _16.py
@@ -26,3 +26,11 @@
   select email 
   from person
   having count(email) > 1
+
+
+5. 183. Customers Who Never Order
+  select Name as Customers
+  from Customers c 
+  left join Orders o
+  on c.Id = o.CustomerId
+  where o.CustomerId is null
