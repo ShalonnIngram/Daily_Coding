5c87b0d14168cc32c6367a571bb1285b4068a1f0
ShalonnIngram <mscianika@gmail.com>
2021-07-18 18:14:53 -0400

    Update 7_18.py

--- Challenges/7_18.py
+++ Challenges/7_18.py
@@ -2,3 +2,10 @@
   select name, population, area
   from world 
   where area > 3000000 or population > 25000000
+  
+2. 197. Rising Temperature
+  select distinct a.id as Id
+  from weather a
+  join weather b
+  where a.temperature > b.temperature
+  and Datediff(a.recordDate, b.recordDate) = 1
