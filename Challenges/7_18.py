0fb20e26f4af8704f635e8c5bbdbe975a1a4382f
ShalonnIngram <mscianika@gmail.com>
2021-07-18 18:28:57 -0400

    Update 7_18.py

--- Challenges/7_18.py
+++ Challenges/7_18.py
@@ -9,3 +9,11 @@
   join weather b
   where a.temperature > b.temperature
   and Datediff(a.recordDate, b.recordDate) = 1
+    
+3. 596. Classes More Than 5 Students
+  select class
+  from
+      (select class, count(distinct student) num
+      from courses
+      group by class) temp
+  where num >= 5
