c6c3b13370ca118a57a8d9fc3af0b018392fbcca
ShalonnIngram <mscianika@gmail.com>
2021-08-04 14:40:31 -0400

    Update August_4.sql

--- Challenges/August_4.sql
+++ Challenges/August_4.sql
@@ -3,3 +3,11 @@
   select player_id, min(event_date) as first_login
   from activity
   group by player_id
+  
+  
+512. Game Play Analysis II 
+  select player_id, device_id
+  from
+    (select player_id, min(event_date), device_id
+    from activity
+    group by 1) sub
