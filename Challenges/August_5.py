baa88842287777b3099e7a70f48ec06f08d94070
ShalonnIngram <mscianika@gmail.com>
2021-08-05 16:33:49 -0400

    Update August_5.py

--- Challenges/August_5.py
+++ Challenges/August_5.py
@@ -9,3 +9,11 @@ class Solution:
             return True
         else:
             return False
+
+        
+1. Two Sumclass Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
