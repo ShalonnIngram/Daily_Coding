ae913dbad505adea50625de5fe9e5ca1adecbaeb
ShalonnIngram <mscianika@gmail.com>
2021-08-06 09:59:01 -0400

    Update August_6.sql

--- Challenges/August_6.sql
+++ Challenges/August_6.sql
@@ -4,3 +4,28 @@ select user_id,
 from logins
 where year(time_stamp) = '2020'
 group by 1
+
+
+
+1795. Rearrange Products Table
+select product_id,
+      'store1' store,
+       store1 price
+from products
+where store1 is not null
+
+union
+
+select product_id,
+      'store2' store,
+       store2 price
+from products
+where store2 is not null
+
+union
+
+select product_id,
+      'store3' store,
+      store3 price
+from products
+where store3 is not null
