38c7ba495552c5760dc45fe83fa7d44abf946565
ShalonnIngram <mscianika@gmail.com>
2021-08-06 15:09:05 -0400

    Update August_6.sql

--- Challenges/August_6.sql
+++ Challenges/August_6.sql
@@ -116,3 +116,12 @@ from (
     from Project
     group by 1) a
 where rnk = 1
+
+
+
+1148. Article Views I
+select distinct author_id as id
+from views
+where author_id = viewer_id
+order by 1 asc
+
