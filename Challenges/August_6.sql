0547d6e6c4037d09e5a9207ffe640d23bd1d84b5
ShalonnIngram <mscianika@gmail.com>
2021-08-06 14:17:09 -0400

    Update August_6.sql

--- Challenges/August_6.sql
+++ Challenges/August_6.sql
@@ -6,6 +6,7 @@ where year(time_stamp) = '2020'
 group by 1
 
 
+
 1795. Rearrange Products Table
 select product_id,
       'store1' store,
@@ -30,6 +31,7 @@ from products
 where store3 is not null
 
 
+
 1083. Sales Analysis II
 select distinct buyer_id
 from sales s
@@ -44,13 +46,27 @@ where product_name = 'S8' and buyer_id not in
 
 
 
+
 1757. Recyclable and Low Fat Products
 select product_id
 from products p
 where low_fats = 'Y' and recyclable = 'Y'
 
 
+
 1069. Product Sales Analysis II
 select product_id, sum(quantity) total_quantity
 from sales
 group by 1
+
+
+
+1050. Actors and Directors Who Cooperated At Least Three Times
+with cte as (
+    select actor_id, count(actor_id) act_count, director_id, count(director_id) dir_count
+    from actordirector
+    group by 1
+)
+select actor_id, director_id
+from cte
+where act_count >= 3 and dir_count >= 3
