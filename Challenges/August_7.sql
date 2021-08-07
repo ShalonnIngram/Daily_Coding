fc6ec3732da53e9a38bbde884aba774b6ed0124f
ShalonnIngram <mscianika@gmail.com>
2021-08-07 08:50:09 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -14,3 +14,17 @@ group by 1
 select tweet_id
 from cte
 where content_len > 15
+
+
+1587. Bank Account Summary II
+with cte as (
+select name, sum(amount) balance
+from users u
+join transactions t
+on u.account = t.account
+group by 1
+)
+
+select name, balance
+from cte
+where balance > 10000
