e1c4c1b6a79833ab00904e6d7ec20b9100c6e961
ShalonnIngram <mscianika@gmail.com>
2021-08-07 08:42:53 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -3,3 +3,14 @@
 select user_id, count(follower_id) followers_count
 from followers
 group by 1
+
+
+1683. Invalid Tweets
+with cte as (
+select tweet_id, length(content) content_len
+from tweets
+group by 1
+)
+select tweet_id
+from cte
+where content_len > 15
