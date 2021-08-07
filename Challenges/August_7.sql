49f8120e1b3afec8b1e61ef2ac3dc8b6bc81ea56
ShalonnIngram <mscianika@gmail.com>
2021-08-07 14:58:48 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -35,3 +35,18 @@ where balance > 10000
 select score,
 dense_rank() over (order by score desc) `Rank`
 from scores
+
+
+184. Department Highest Salary
+select Department, 
+       Employee,
+       Salary
+from
+    (select d.name Department,
+           e.name Employee,
+           Salary,
+           dense_rank() over (partition by d.name order by salary desc) as rnk
+    from employee e
+    join department d
+    on e.departmentid = d.id) sub
+where rnk = 1 
