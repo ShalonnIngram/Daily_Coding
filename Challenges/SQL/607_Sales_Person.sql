c06fa5d22b4f10bd83d564ccf8b4281a22ce9335
ShalonnIngram <mscianika@gmail.com>
2021-08-12 10:28:06 -0400

    Create 607_Sales_Person.sql

---
+++ Challenges/SQL/607_Sales_Person.sql
@@ -0,0 +1,9 @@
+607. Sales Person
+select name 
+from salesperson
+where sales_id not in
+    (select o.sales_id
+    from company c
+    join orders o
+    on c.com_id = o.com_id
+    where c.name = 'RED')
