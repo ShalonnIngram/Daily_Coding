68153fe4f6c24f94ce4dbab578a0fc8d62c1d105
ShalonnIngram <mscianika@gmail.com>
2021-09-09 22:04:42 -0400

    Create 1077_Project_Employees_III.sql

---
+++ Challenges/SQL/1077_Project_Employees_III.sql
@@ -0,0 +1,13 @@
+with cte as(
+select p.project_id project_id,
+       p.employee_id employee_id,
+       experience_years,
+       dense_rank() over (partition by project_id order by experience_years desc) rnk
+from project p
+left join employee e
+on p.employee_id = e.employee_id
+)
+select project_id,
+       employee_id
+from cte
+where rnk = 1
