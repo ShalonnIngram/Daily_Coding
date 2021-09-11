a97e091ecdf8bc0cafcd8158967f3ca751da65a7
ShalonnIngram <mscianika@gmail.com>
2021-09-10 23:13:36 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -1,14 +1,4 @@
 
-1683. Invalid Tweets
-with cte as (
-select tweet_id, length(content) content_len
-from tweets
-group by 1
-)
-select tweet_id
-from cte
-where content_len > 15
-
 
 1587. Bank Account Summary II
 with cte as (
