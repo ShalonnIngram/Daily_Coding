459def24a4b4a608d4fbf1771278914394eecf41
ShalonnIngram <mscianika@gmail.com>
2021-09-15 23:16:48 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -1,20 +1,4 @@
 
-
-1587. Bank Account Summary II
-with cte as (
-select name, sum(amount) balance
-from users u
-join transactions t
-on u.account = t.account
-group by 1
-)
-
-select name, balance
-from cte
-where balance > 10000
-
-
-
 178. Rank Scores
 select score,
 dense_rank() over (order by score desc) `Rank`
