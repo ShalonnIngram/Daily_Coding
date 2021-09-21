19bebaa43cb5f85b773537a189c216d86d06b95d
ShalonnIngram <mscianika@gmail.com>
2021-09-20 22:58:45 -0400

    Update 184_Department_Highest_Salary.sql

--- Challenges/SQL/184_Department_Highest_Salary.sql
+++ Challenges/SQL/184_Department_Highest_Salary.sql
@@ -1,2 +1,12 @@
-
-184_Department_Highest_Salary.sql
+select Department, 
+       Employee,
+       Salary
+from
+    (select d.name Department,
+           e.name Employee,
+           Salary,
+           dense_rank() over (partition by d.name order by salary desc) as rnk
+    from employee e
+    join department d
+    on e.departmentid = d.id) sub
+where rnk = 1 
