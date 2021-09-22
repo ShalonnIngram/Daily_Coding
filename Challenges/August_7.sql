28e4568e38be8b8988515ec6b290971d4e93f770
ShalonnIngram <mscianika@gmail.com>
2021-09-21 22:38:42 -0400

    Update August_7.sql

--- Challenges/August_7.sql
+++ Challenges/August_7.sql
@@ -1,14 +1,1 @@
 
-184. Department Highest Salary
-select Department, 
-       Employee,
-       Salary
-from
-    (select d.name Department,
-           e.name Employee,
-           Salary,
-           dense_rank() over (partition by d.name order by salary desc) as rnk
-    from employee e
-    join department d
-    on e.departmentid = d.id) sub
-where rnk = 1 
