6708740ffedd2c14ce524e60722e69cd5df4be7b
ShalonnIngram <mscianika@gmail.com>
2021-09-28 22:51:15 -0400

    Update 1677_Products_Worth_Over_Invoices.sql

--- Challenges/SQL/1677_Products_Worth_Over_Invoices.sql
+++ Challenges/SQL/1677_Products_Worth_Over_Invoices.sql
@@ -1,10 +1,10 @@
 select name,
-       sum(rest) rest,
-       sum(paid) paid,
-       sum(canceled) canceled,
-       sum(refunded) refunded
+       ifnull(sum(rest),0) rest,
+       ifnull(sum(paid),0) paid,
+       ifnull(sum(canceled),0) canceled,
+       ifnull(sum(refunded),0) refunded
 from product p
-join invoice i
+left join invoice i
 on p.product_id = i.product_id
 group by 1
 order by 1
