5063b62878acfc02ef5657f91676eaabb9e54829
ShalonnIngram <mscianika@gmail.com>
2021-12-13 22:06:16 -0500

    Update 1_two_sum.py

--- Challenges/Python/1_two_sum.py
+++ Challenges/Python/1_two_sum.py
@@ -4,3 +4,12 @@
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+                
+                
+                
+def pair_sum(numbers, target_sum):
+  for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+      if numbers[i] + numbers[j] == target_sum:
+        return (i, j)
