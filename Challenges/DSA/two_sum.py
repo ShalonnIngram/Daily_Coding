d1540781d140f7c77c9244efdd731487d38c673f
ShalonnIngram <mscianika@gmail.com>
2021-12-15 22:45:40 -0500

    Update two_sum.py

--- Challenges/DSA/two_sum.py
+++ Challenges/DSA/two_sum.py
@@ -1,6 +1,29 @@
-
+# BRUTE FORCE APPROACH
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+
+                
+nums = [2, 1, 9, 22, 41, 52, 12]
+target = 13
+
+# create an empty hash map
+# iterate over the list, we need the index & value
+# create a difference variable that returns the difference between the target & the value
+# if the difference is in the hashmap, return the indicies
+# else update the hashmap
+
+
+def twosum(nums, target):
+    mymap = {}     # mapping value: index
+
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in mymap:
+            return [mymap[diff], i]   # pair of indicies
+        mymap[n] = i      # update hashmap
+
+print(twosum(nums, target))
