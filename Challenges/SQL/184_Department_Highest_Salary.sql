288bbf55916deb6a21ea4016c6df9b212779ec24
ShalonnIngram <mscianika@gmail.com>
2023-11-03 21:51:39 -0400

    Update 184_Department_Highest_Salary.sql

--- Challenges/SQL/184_Department_Highest_Salary.sql
+++ Challenges/SQL/184_Department_Highest_Salary.sql
@@ -10,3 +10,21 @@ from
     join department d
     on e.departmentid = d.id) sub
 where rnk = 1 
+
+
+
+
+
+with sal_rank as (
+select name,
+       salary,
+       departmentId,
+       dense_rank() over(partition by departmentId order by salary desc) as rnk_sal
+from employee
+)
+select d.name as Department,
+       s.name as Employee, 
+       s.salary as Salary
+from sal_rank s 
+left join department d on s.departmentId = d.id
+where rnk_sal = 1
