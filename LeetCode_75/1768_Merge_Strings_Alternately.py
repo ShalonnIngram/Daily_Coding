ac2951ec308ebaba69a61eaa40a8d97595a20d35
ShalonnIngram <mscianika@gmail.com>
2023-11-09 11:02:48 -0500

    Update 1768_Merge_Strings_Alternately.py

--- LeetCode_75/1768_Merge_Strings_Alternately.py
+++ LeetCode_75/1768_Merge_Strings_Alternately.py
@@ -1,1 +1,23 @@
 
+
+- initialize the pointers, add characters to array then join 
+1. While Loop  - iterate over each of the words, while in bounds, and add to list 
+2. append each of the characters to a list
+3. Add to the counter
+4. ensure all of the characters are added by appending (by index) any remaining characters
+5. join all of the characters together in the list 
+
+
+    def mergeAlternately(self, word1, word2):
+        i, j = 0, 0
+
+        res = []
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[i])
+            i += 1
+            j += 1
+
+        res.append(word1[i:])
+        res.append(word2[j:])
+        return "".join(res)
